f46bbf619fb14a0a8ec007467e1474152bb48b48
apsoft <1790042182@qq.com>
2025-04-14 16:36:04 +0800

    _25-04-14-16-36-01，地点，人物，事件，前因，后果，影响力
    _25-04-14-16-36-02，地点，人物，事件，前因，后果，影响力

--- gongyongmokuai.js
+++ gongyongmokuai.js
@@ -16,13 +16,13 @@ function ts_ask(msg){
 
 }
 
-function ts_input(prompt, content){
+function ts_input(tprompt, tcontent){
 
 
 
 
 
-    return prompt(prompt,content);
+    return prompt(tprompt, tcontent);
 
 }
 
@@ -108,7 +108,7 @@ function str_encrypt(str, pw){
 
 	tstr = 加密操作1.rc4加密(str, pw);
 
-	return(tstr);
+	return tstr;
 
 }
 
@@ -128,7 +128,7 @@ function str_decrypt(str, pw){
 
 	tstr = 加密操作1.rc4解密(str, pw);
 
-	return(tstr);
+	return tstr;
 
 }
 
@@ -152,7 +152,7 @@ function str_timenow(){
 
 	tstr = 文本操作.子文本替换(tstr, ":", "");
 
-	return(tstr);
+	return tstr;
 
 }
 
@@ -204,6 +204,92 @@ function str_isnumber(str){
 
 }
 
+function net_getmid(tstr){
+
+
+
+
+
+	var tstr1;
+
+	var tstrs;
+
+	if(tstr == "" ){
+
+		return "";
+
+	}
+
+	tstr1 = 公用模块.str_getmid(tstr, "\\[ONLINEHOME\\]","\\[ONLINEEND\\]");
+
+	if(tstr1 != 0 ){
+
+		tstr1 = 公用模块.str_decrypt(tstr1, "fucksjg");
+
+		return tstr1;
+
+	}
+
+}
+
+function net_iscampus(tstr){
+
+
+
+
+
+
+
+
+
+	var tstr1;
+
+	var tstrs;
+
+	var tlogstr= "否";
+
+	tstr1 = tstr;
+
+	if(tstr1 != "" ){
+
+		公用模块.trace("取到的超星源码不为空");
+
+		tstrs = 文本操作.取指定文本(tstr1, "找到相关的", "种");
+
+		if(数组操作.取成员数(tstrs) != 0 ){
+
+			tstr1 = tstrs[0] + "种";
+
+			tstrs = 文本操作.取指定文本(tstr1, "<b>", "种");
+
+			if(数组操作.取成员数(tstrs) != 0 ){
+
+				tstr1 = tstrs[0];
+
+				tstr1 = 文本操作.删首尾空(tstr1);
+
+				if(tstr1 != "" ){
+
+					tlogstr = "是";
+
+				}
+
+			}
+
+		}
+
+	}else{
+
+		tlogstr = "否";
+
+	}
+
+	读写设置.保存设置("校园网", tlogstr);
+
+	return tlogstr;
+
+}
+
 function time_wait(ms){
 
 
@@ -242,6 +328,12 @@ function str_cnblogsurl(url){
 
 	tstr = url;
 
+	if(文本操作.寻找文本(tstr, "cnblogs", 0) == -1 ){
+
+		return tstr;
+
+	}
+
 
 
 	tstrs = 文本操作.取指定文本(tstr, "t=", "&download");
@@ -250,7 +342,7 @@ function str_cnblogsurl(url){
 
 		tstr = tstrs[0];
 
-		console.log("取到的中间文本：" + tstr);
+
 
 		tstr2 = tstr;
 
@@ -274,31 +366,31 @@ function str_cnblogsurl(url){
 
 
 
+		return tstr;
+
 	}else{
 
-		tstr = url;
+		return tstr;
 
 
 
 	}
 
-	return tstr;
-
 }
 
-function str_getmid(str, begin, end){
+function str_getmid(tstr, begin, end){
 
-	var tstr;
+	var tstr1;
 
 	var tstrs;
 
-	tstrs = 文本操作.取指定文本(str, begin, end);
+	tstrs = 文本操作.取指定文本(tstr, begin, end);
 
 	if(数组操作.取成员数(tstrs) != 0 ){
 
-		tstr = tstrs[0];
+		tstr1 = tstrs[0];
 
-		return tstr;
+		return tstr1;
 
 	}else{
 
@@ -334,7 +426,7 @@ function num_isinarray(num,array){
 
 }
 
-function num_getrandom(min, max, count){
+function num_getrandomarray(min, max, count){
 
 	var tarray = [];
 
@@ -361,6 +453,472 @@ function num_getrandom(min, max, count){
 	return tarray;
 
 }
+
+function str_stripquote(str){
+
+	var tstr;
+
+
+
+
+
+	tstr = str;
+
+	if(文本操作.取文本左边(tstr, 1) == 转换操作.代码转字符(34) ){
+
+		tstr = 文本操作.取文本右边(tstr, 文本操作.取文本长度(tstr)-1);
+
+	}
+
+	if(文本操作.取文本右边(tstr, 1) == 转换操作.代码转字符(34) ){
+
+		tstr = 文本操作.取文本左边(tstr, 文本操作.取文本长度(tstr)-1);
+
+	}
+
+	if(文本操作.取文本左边(tstr, 1) == 转换操作.代码转字符(39) ){
+
+		tstr = 文本操作.取文本右边(tstr, 文本操作.取文本长度(tstr)-1);
+
+	}
+
+	if(文本操作.取文本右边(tstr, 1) == 转换操作.代码转字符(39) ){
+
+		tstr = 文本操作.取文本左边(tstr, 文本操作.取文本长度(tstr)-1);
+
+	}
+
+	return tstr;
+
+}
+
+function domp_gets1(thtml, css1, css2, indexfromcss2, getinnertext, attribute, include, exclude, replacea, withb){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	var tobjs = [];
+
+	var tobjs1 = [];
+
+	var tobjs2 = [];
+
+	var thtml1 = [];
+
+	var rest= "";
+
+	var tobj;
+
+	var rests = [];
+
+	var rests1 = [];
+
+	数组操作.清空数组(tobjs);
+
+	if(css2 == "" ){
+
+
+
+
+
+		tobjs = domp_html2objs(thtml, css1);
+
+
+
+	}else{
+
+
+
+		thtml1 = domp_html2htmls(thtml, css1);
+
+
+
+
+
+		mui.each(thtml1,function(索引,成员){
+
+			if(indexfromcss2 == -1 ){
+
+
+
+				tobjs1 = domp_html2objs(成员, css2);
+
+
+
+			}else{
+
+
+
+				数组操作.清空数组(tobjs1);
+
+				tobjs2 = domp_html2objs(成员, css2);
+
+				tobj = tobjs2[indexfromcss2];
+
+				数组操作.加入尾成员(tobjs1, tobj);
+
+			}
+
+			mui.each(tobjs1,function(索引,成员){
+
+				数组操作.加入尾成员(tobjs, 成员);
+
+			})
+
+
+
+		})
+
+	}
+
+
+
+	数组操作.清空数组(rests1);
+
+	mui.each(tobjs,function(索引,成员){
+
+		if(getinnertext == true ){
+
+			rest = 成员.innerText;
+
+		}else{
+
+			rest = 成员.getAttribute(attribute);
+
+		}
+
+
+
+		数组操作.加入尾成员(rests1, rest);
+
+	})
+
+	数组操作.清空数组(rests);
+
+	mui.each(rests1,function(索引,成员){
+
+
+
+		if(str_isinexclude(成员, include, exclude) == true ){
+
+			if(数组操作.取成员数(replacea) == 0 || 数组操作.取成员数(withb) == 0 ){
+
+
+
+				rest = 成员;
+
+			}else{
+
+				rest = str_replace(成员, replacea, withb);
+
+			}
+
+			数组操作.加入尾成员(rests, rest);
+
+		}
+
+	})
+
+	return rests;
+
+}
+
+function domp_html2htmls(thtml, css){
+
+
+
+	const parser = new DOMParser();
+
+	var doc;
+
+	var tobjs;
+
+	var rets = [];
+
+
+
+	doc = parser.parseFromString(thtml, "text/html");
+
+	tobjs = doc.querySelectorAll(css);
+
+	mui.each(tobjs,function(索引,成员){
+
+
+
+		数组操作.加入尾成员(rets, 成员.innerHTML);
+
+	})
+
+
+
+	return rets;
+
+}
+
+function domp_html2objs(thtml, css){
+
+
+
+	const parser = new DOMParser();
+
+	var doc;
+
+	var tobjs;
+
+	doc = parser.parseFromString(thtml, "text/html");
+
+	tobjs = doc.querySelectorAll(css);
+
+	return tobjs;
+
+}
+
+function domp_html2innertexts(thtml, css){
+
+
+
+	const parser = new DOMParser();
+
+	var doc;
+
+	var tobjs;
+
+	var rets = [];
+
+
+
+	doc = parser.parseFromString(thtml, "text/html");
+
+	tobjs = doc.querySelectorAll(css);
+
+	mui.each(tobjs,function(索引,成员){
+
+
+
+		数组操作.加入尾成员(rets, 成员.innerText);
+
+	})
+
+	return rets;
+
+}
+
+function domp_html2attributes(thtml, css, attribute){
+
+
+
+	const parser = new DOMParser();
+
+	var doc;
+
+	var tobjs;
+
+	var rets = [];
+
+
+
+	doc = parser.parseFromString(thtml, "text/html");
+
+	tobjs = doc.querySelectorAll(css);
+
+	mui.each(tobjs,function(索引,成员){
+
+		数组操作.加入尾成员(rets, 成员.getAttribute(attribute));
+
+	})
+
+	return rets;
+
+}
+
+function str_isinexclude(tstr, include, exclude){
+
+	if(文本操作.删首尾空(tstr) == "" ){
+
+		return false;
+
+	}
+
+	if(include == "" ){
+
+
+
+		if(exclude == "" ){
+
+
+
+
+
+			return true;
+
+		}else{
+
+
+
+			if(文本操作.寻找文本(tstr, exclude, 0) != -1 ){
+
+
+
+				return false;
+
+			}else{
+
+
+
+				return true;
+
+			}
+
+		}
+
+	}else{
+
+
+
+
+
+		if(文本操作.寻找文本(tstr, include) == -1 ){
+
+			return false;
+
+		}else{
+
+
+
+
+
+			if(exclude == "" ){
+
+
+
+
+
+				return true;
+
+			}else{
+
+
+
+				if(文本操作.寻找文本(tstr, exclude, 0) == -1 ){
+
+
+
+					return true;
+
+				}else{
+
+
+
+					return false;
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
+
+function str_replace(tstr, tstr1, tstr2){
+
+
+
+
+
+	if(数组操作.取成员数(tstr1) == 0 ){
+
+		return tstr;
+
+	}
+
+	if(数组操作.取成员数(tstr2) == 0 ){
+
+		return tstr;
+
+	}
+
+	if(数组操作.取成员数(tstr1) != 数组操作.取成员数(tstr2) ){
+
+		return tstr;
+
+	}
+
+	mui.each(tstr1,function(索引,成员){
+
+		tstr = 文本操作.子文本替换(tstr, 成员, tstr2[索引]);
+
+	})
+
+	return tstr;
+
+}
+
+function str_joinarray(tstrs, witha){
+
+	var res= "";
+
+	if(数组操作.取成员数(tstrs) == 0 ){
+
+		return "";
+
+	}
+
+	mui.each(tstrs,function(索引,成员){
+
+		if(索引 == 0 ){
+
+			res = 成员;
+
+		}else{
+
+			res = res + witha + 成员;
+
+		}
+
+	})
+
+	return res;
+
+}
+
+function import_jssrc(thref){
+
+	var JS = document.createElement("script");
+
+
+
+	JS.src = thref;
+
+	document.getElementsByTagName("head").item(0).appendChild(JS);
+
+}
+
+function import_jstext(ttext){
+
+	var JS = document.createElement("script");
+
+	JS.type = "text/javascript";
+
+	JS.text = tstr;
+
+	document.getElementsByTagName("head").item(0).appendChild(JS);
+
+}
 window["公用模块"]["ts_lnbox"]=ts_lnbox;
 window["公用模块"]["ts_ask"]=ts_ask;
 window["公用模块"]["ts_input"]=ts_input;
@@ -371,9 +929,22 @@ window["公用模块"]["str_decrypt"]=str_decrypt;
 window["公用模块"]["str_timenow"]=str_timenow;
 window["公用模块"]["str_getrandomnumber"]=str_getrandomnumber;
 window["公用模块"]["str_isnumber"]=str_isnumber;
+window["公用模块"]["net_getmid"]=net_getmid;
+window["公用模块"]["net_iscampus"]=net_iscampus;
 window["公用模块"]["time_wait"]=time_wait;
 window["公用模块"]["str_cnblogsurl"]=str_cnblogsurl;
 window["公用模块"]["str_getmid"]=str_getmid;
 window["公用模块"]["num_isinarray"]=num_isinarray;
-window["公用模块"]["num_getrandom"]=num_getrandom;
+window["公用模块"]["num_getrandomarray"]=num_getrandomarray;
+window["公用模块"]["str_stripquote"]=str_stripquote;
+window["公用模块"]["domp_gets1"]=domp_gets1;
+window["公用模块"]["domp_html2htmls"]=domp_html2htmls;
+window["公用模块"]["domp_html2objs"]=domp_html2objs;
+window["公用模块"]["domp_html2innertexts"]=domp_html2innertexts;
+window["公用模块"]["domp_html2attributes"]=domp_html2attributes;
+window["公用模块"]["str_isinexclude"]=str_isinexclude;
+window["公用模块"]["str_replace"]=str_replace;
+window["公用模块"]["str_joinarray"]=str_joinarray;
+window["公用模块"]["import_jssrc"]=import_jssrc;
+window["公用模块"]["import_jstext"]=import_jstext;
 })();
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 mui.init({swipeBack: false
 ,gestureConfig: {tap:true,doubletap:true,longtap:true,hold:true,release:true}});
 
-var 标题栏1 = new 标题栏("标题栏1",null,标题栏1_右侧图标被单击,标题栏1_左侧图标被单击);
-var 编辑框1 = new 编辑框("编辑框1",null,null,null,null,null);
+var 标题栏1 = new 标题栏("标题栏1",null,标题栏1_右侧图标被单击,null);
+var 浏览框1 = new 浏览框("浏览框1",null);
+var 浏览框2 = new 浏览框("浏览框2",null);
+var 浏览框3 = new 浏览框("浏览框3",null);
+var 浏览框4 = new 浏览框("浏览框4",null);
+var 浏览框5 = new 浏览框("浏览框5",null);
 var 按钮组1 = new 按钮组("按钮组1",按钮组1_被单击);
-var 按钮1 = new 按钮("按钮1",按钮1_被单击,null,null);
-var 编辑框2 = new 编辑框("编辑框2",null,null,null,null,null);
-var 图片框1 = new 图片框("图片框1",null);
-var 网络操作1 = new 网络操作("网络操作1",网络操作1_发送完毕);
-var 加密操作1 = new 加密操作("加密操作1");
-var 剪贴板1 = new 剪贴板("剪贴板1");
 var 菜单1 = new 菜单("菜单1",菜单1_菜单项被单击);
 if(mui.os.plus){
     mui.plusReady(function() {
@@ -24,133 +22,112 @@ if(mui.os.plus){
 }
 
 function 主窗口_创建完毕(){
-
-
-	图片框1.置图片("https://qr.api.cli.im/qr/index/jsonp_generate?data=https://qr05.cn/Cb729V&kid=cliim");
-
-	部署按钮组1();
-	部署菜单();
+	部署底部按钮组();
+	部署浏览框();
+	部署整体菜单();
 
 }
-function 部署菜单(){
+function 部署浏览框(){
 
+
+
+	var theight;
+	theight = 窗口操作.取窗口高度()-45-40-12;
+
+	浏览框1.置尺寸("100%", theight+"px");
+	浏览框2.置尺寸("100%", theight+"px");
+	浏览框3.置尺寸("100%", theight+"px");
+	浏览框4.置尺寸("100%", theight+"px");
+	浏览框5.置尺寸("100%", theight+"px");
+	浏览框1.置可视(true);
+	浏览框2.置可视(false);
+	浏览框3.置可视(false);
+	浏览框4.置可视(false);
+	浏览框5.置可视(false);
+	浏览框1.跳转("https://apsoft-ai.github.io/dsson/index.html");
+	浏览框2.跳转("https://mfindsdnu.libsp.cn/#/Home");
+	浏览框3.跳转("https://apsoft-ai.github.io/ds-cnki/index.html");
+	浏览框4.跳转("https://www.cnki.net");
+	浏览框5.跳转("https://apsoft-ai.github.io/ds-find-cnki/about.html");
+}
+function 部署底部按钮组(){
+	按钮组1.置标题(0, "深求发现");
+	按钮组1.置标题(1, "发现");
+	按钮组1.置标题(2, "深求知网");
+	按钮组1.置标题(3, "知网");
+}
+function 部署整体菜单(){
 	菜单1.清空菜单项();
 
-	菜单1.添加菜单项("示例");
+	菜单1.添加菜单项("关于");
 
 
 
-	菜单1.添加菜单项("通用爬虫");
+
 
 }
-
-function 按钮1_被单击(){
-
-	if(编辑框1.取内容() == "" ){
-
-		编辑框2.置内容("请先输入或粘贴问题进来！");
-
-		return;
-
-	}
-
-	deepseek_silicon(编辑框1.取内容());
-
-}
-
-function deepseek_silicon(pt){
-
-	var url;
-
-	var key;
-
-	编辑框2.置内容("DeepSeek-R1-Distill-Qwen-7B思考中……\n\n非流式输出，耗时稍长，请耐心等待。");
-
-	url = "https://api.siliconflow.cn/v1/chat/completions";
-
-	key = "U2FsdGVkX1+g8bGdev2rslxNyvr3gbKAtin8jpVTluXdTNTdFXiBKAcSFYh07AwSjUaSzcka/RZ2kZGtFJwVJRnhHA==";
-
-	key = 公用模块.str_decrypt(key, "fucksjg");
-
-	pt = pt + "\n以上是文献检索需求，请告诉我在【中国知网】中检索相关文献应该如何设置检索词，如何选择检索入口等，尽量得到好结果。";
-
-	网络操作1.置附加请求头({"Authorization": "Bearer " + key, "Content-Type": "application/json"});
-
-	网络操作1.发送网络请求(url, "post", "json", { "model": "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B", "messages": [ { "role": "user", "content": pt } ], "max_tokens": 16384, "temperature": 0.7, "top_p": 0.7, "top_k": 50, "frequency_penalty": 0.5, "n": 1, "response_format": {"type": "text"} }, 6000000);
-
-}
-
-function 网络操作1_发送完毕(发送结果,返回信息){
-
-	console.log(返回信息);
-
-
-
-
-
-	编辑框2.置内容(返回信息.choices[0].message.content);
-
-}
-
-function 部署按钮组1(){
-
-	按钮组1.置标题(0,"清空");
-
-	按钮组1.置标题(1, "复制");
-
-	按钮组1.置标题(2, "示例");
-
-}
-
 function 按钮组1_被单击(按钮索引){
-
 	switch(按钮索引){
-
 	case 0 :
 
-		编辑框1.置内容("");
-
-		编辑框1.获取焦点();
-
+		浏览框1.置可视(true);
+		浏览框2.置可视(false);
+		浏览框3.置可视(false);
+		浏览框4.置可视(false);
+		浏览框5.置可视(false);
+		标题栏1.置标题("DeepSeek");
 	break;
 	case 1 :
 
-		剪贴板1.置剪贴板内容(编辑框1.取内容());
+		浏览框1.置可视(false);
+		浏览框2.置可视(true);
+		浏览框3.置可视(false);
+		浏览框4.置可视(false);
+		浏览框5.置可视(false);
+		标题栏1.置标题("图书馆发现系统");
+	break;
+	case 2 :
 
+		浏览框1.置可视(false);
+		浏览框2.置可视(false);
+		浏览框3.置可视(true);
+		浏览框4.置可视(false);
+		浏览框5.置可视(false);
+		标题栏1.置标题("DeepSeek - cnki");
+	break;
+	case 3 :
 
-
-
-
+		浏览框1.置可视(false);
+		浏览框2.置可视(false);
+		浏览框3.置可视(false);
+		浏览框4.置可视(true);
+		浏览框5.置可视(false);
+		标题栏1.置标题("中国知网");
 		break;
 	}
-
-}
-function 标题栏1_左侧图标被单击(){
-
-	窗口操作.切换窗口("index.html");
-
 }
 function 标题栏1_右侧图标被单击(){
-
 	菜单1.显示菜单();
-
 }
 function 菜单1_菜单项被单击(菜单项标题){
 
 	switch(菜单项标题){
 
-	case "示例" :
+	case "关于" :
 
-		编辑框1.置内容("想在“中国知网”中检索与“信息素养”相关文献，要求如下：\n近5年内论文，太老的文献不需要；\n文章内容突出“人工智能”或“元宇宙”或“智能检索”；\n文章需提到“师范大学图书馆信息素养课程”。");
+		浏览框1.置可视(false);
+		浏览框2.置可视(false);
+		浏览框3.置可视(false);
+		浏览框4.置可视(false);
+		浏览框5.置可视(true);
 
 
 
 
 
-	break;
-	case "通用爬虫" :
 
-		窗口操作.打开指定网址("https://affiliate.bazhuayu.com/M8lKUC", 2);
+
+
 
 		break;
 	}
